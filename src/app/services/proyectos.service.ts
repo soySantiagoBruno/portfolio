1283a4135023ea11011bc0306923abbbd9a9215d
Santiago <soysantiagobruno@gmail.com>
2025-04-22 22:47:27 -0300

    proyecto service

--- src/app/services/proyectos.service.ts
+++ src/app/services/proyectos.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProyectosService {
 
-  constructor() { }
+  private baseUrl: string = 'http://localhost:8881/wp-json/wp/v2'
 
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/posts`)
+  }
   
 }
