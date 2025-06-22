02ea34454902d1c448d821edac890c9dc027780a
Santiago Bruno <soysantiagobruno@gmail.com>
2025-06-22 16:53:05 -0300

    hago el footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { InfoGeneralComponent } from "./components/info-general/info-general.component";
 import { ModalProyectoComponent } from "./components/carrusel/modal-proyecto/modal-proyecto.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CarruselComponent, InfoGeneralComponent, ModalProyectoComponent],
+  imports: [CarruselComponent, InfoGeneralComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
