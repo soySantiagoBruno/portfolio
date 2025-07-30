dbbf20f4b40c260b2a932542be2f6319018e7dd0
Santiago Bruno <soysantiagobruno@gmail.com>
2025-07-30 00:53:22 -0300

    agrego tipo Persona

---
+++ src/app/models/persona.ts
@@ -0,0 +1,12 @@
+interface Persona {
+  nombre: string;
+  titulo: string;
+  descripcion: string;
+  urlCv: string;
+  email: string;
+  linkedin: string;
+  github: string;
+  idiomas: string;
+  tecnologiasFrontend: string[];
+  image: string;
+}
